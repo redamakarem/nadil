a678c28d7fa9a45bcc230982350f8a48b1f59794
Reda Makarem <makarem.reda@gmail.com>
2022-06-01 13:50:27 +0300

    Update restaurant-menu.blade.php

--- resources/views/site/mobile/restaurant-menu.blade.php
+++ resources/views/site/mobile/restaurant-menu.blade.php
@@ -6,7 +6,7 @@
                 <h1 class="text-center font-lato  font-medium font-light text-xl text-black uppercase tracking-[1.0rem]">Oskoyo</h1>
                 <h4 class="text-center font-lato  font-medium font-light text-xs text-black uppercase tracking-[0.6rem]">Kuwait city</h4>
                 <h4 class="text-center font-lato  font-medium font-light text-[0.6rem] text-black uppercase tracking-[0.4rem] mt-5">&quot;Curated to unlock new culinary moments &quot;</h4>
-                <h4 class="text-center font-lato font-light text-xs text-black uppercase tracking-[0.5rem] font-bold mt-5">Explore some dishes.</h4>
+                <h4 class="text-center font-lato font-light text-xs text-black uppercase tracking-[0.5rem] font-bold mt-5">Explore some dishes</h4>
             </div>
             <div class="dish-category">
                 <div class="text-center font-lato font-semibold text-xs tracking-[0.4rem] text-black uppercase py-6 px-12 w-full bg-trueGray-200 mt-10 rounded-xl shadow-lg">Salads</div>
