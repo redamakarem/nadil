a278522b6f7d0f4b74fa01f05716c84ebb824e8b
Reda Makarem <makarem.reda@gmail.com>
2022-07-20 15:04:21 +0300

    matomo

--- resources/views/layouts/site-tw.blade.php
+++ resources/views/layouts/site-tw.blade.php
@@ -24,6 +24,22 @@
         }
     </style>
     <title>Nadil</title>
+    <!-- Matomo -->
+<script>
+    var _paq = window._paq = window._paq || [];
+    /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+    _paq.push(['trackPageView']);
+    _paq.push(['enableLinkTracking']);
+    (function() {
+      var u="//nadil-analytics.generalsenses.com/";
+      _paq.push(['setTrackerUrl', u+'matomo.php']);
+      _paq.push(['setSiteId', '1']);
+      var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+      g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
+    })();
+  </script>
+  <!-- End Matomo Code -->
+  
 </head>
 <body class="ltr:font-lato rtl:font-ahlan rtl:tracking-normal rtl:font-normal">
 {{--    <div id="dev" class="bg-cover w-[1920px] h-[1301px] mx-auto bg-slate-100 flex flex-col"--}}
