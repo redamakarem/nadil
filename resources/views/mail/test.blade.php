cfb6db3e6fce1ed5c932b5bead59051cbacd2297
Reda Makarem <makarem.reda@gmail.com>
2022-07-30 15:32:46 +0300

    updated email template

--- resources/views/mail/test.blade.php
+++ resources/views/mail/test.blade.php
@@ -1,28 +1,52 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 
+<head>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,300;0,900;1,100;1,300;1,400;1,700;1,900&display=swap" rel="stylesheet"> 
+</head>
 
-
-<body>
-    <table id="outer" width="80%" style="margin: 0 auto; background-color: #000;">
-        <tr style="height: 200px;">
-            <td>Nadil</td>
-            <td>Cutlery</td>
+<body style="font-family: 'Lato', sans-serif; font-size: 24px; font-weight: 700;">
+    <table id="outer" width="100%" style="margin: 0 auto; background-color: #000;">
+        <tr style="">
+            <td style="text-align: center;"><img style="margin: auto;" src="https://nadil.generalsenses.com/images/logo@2x.png" alt=""></td>
+            <td></td>
         </tr>
         <tr>
             <td colspan="2">
                 <table id="white-part" width="100%" 
-                style="background-color:#FFF; border-top-left-radius: 96px;border-top-right-radius: 96px;min-height: 100px;" >
+                style="background-color:#FFF; border-radius: 96px;min-height: 100px;" >
                     <tr>
-                        <td colspan="2"  style="padding: 40px 0px; text-align: center;">
+                        <td colspan="2"  style="padding: 80px 0px; text-align: center;">
                             <table id="grey-part" width="90%" style="background-color: #EDEFF0;margin:auto;border-radius: 96px;min-height: 200px;">
                                 <tr>
-                                    <td colspan="2" style="text-align: center;">Hello {{$user->name}}</td>
+                                    <td colspan="2" style="text-align: center;text-transform: uppercase;padding-top: 80px;">Hi {{$user->name}}</td>
+                                </tr>
+                                <tr>
+                                    <td colspan="2" style="text-align: center;text-transform: uppercase;padding:30px 0px">Welcome to nadil</td>
+                                </tr>
+                                <tr>
+                                    <td colspan="2" style="text-align: center;text-transform: uppercase;padding:30px 0px">Thank you <br /> for signing up with us</td>
+                                </tr>
+                                <tr>
+                                    <td colspan="2" style="text-align: center;text-transform: uppercase;padding:30px 0px;padding-bottom: 80px;">We cant wait for <br /> your booking experience</td>
                                 </tr>
                             </table>
                         </td>
                     </tr>
+                    <tr>
+                        <td colspan="2" style="text-align:center; font-family: 'Lato', sans-serif; font-size: 24px; text-transform: uppercase;padding-bottom: 30px;">&quot;Curated to provide you <br /> with the best&quot;</td>
+                    </tr>
+                    <tr>
+                        <td colspan="2" style="text-align:center; font-family: 'Lato', sans-serif; font-size: 24px; text-transform: uppercase;"><img src="https://nadil.generalsenses.com/images/insta@2x.png" alt=""></td>
+                    </tr>
                 </table>
             </td>
+        </tr>
+        <tr>
+            <td colspan="1" style="color:#fff;text-transform: uppercase;padding:20px 0px;">Cancellation Policy</td>
+            <td colspan="1" style="color:#fff;text-transform: uppercase;padding:20px 0px">Contact Us</td>
+            <td colspan="1" style="color:#fff;text-transform: uppercase;padding:20px 0px">FAQ</td>
         </tr>
     </table>
 </body>
