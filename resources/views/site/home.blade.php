eec2eb4b8ba36dd9ae4874dce28f625066ec8d7a
Reda Makarem <makarem.reda@gmail.com>
2022-10-19 12:08:42 +0300

    ..

--- resources/views/site/home.blade.php
+++ resources/views/site/home.blade.php
@@ -52,6 +52,7 @@
                     <div class="flex my-4">
                         <input class="font-lato flex text-center border-none py-6 uppercase bg-nadilBtn-100 outline-none rounded-l-lg" 
                         type="text" name="restaurant_name" id="restaurant_name" placeholder="Date">
+                        <div class="bg-gray-600 w-[1px] h-[8px]"></div>
 
                         <input class="font-lato flex text-center border-none py-6 uppercase bg-nadilBtn-100 outline-none" 
                         type="text" name="search_time" id="search_time" placeholder="Time">
