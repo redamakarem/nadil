40cd733e78826bc9bb249b939b97e0d5b5adff13
Reda Makarem <makarem.reda@gmail.com>
2022-12-31 20:27:43 +0300

    ..

---
+++ resources/views/mail/booking-confirmation.blade.php
@@ -0,0 +1,98 @@
+<body id="i3zr" style="box-sizing: border-box; margin: 0;">
+  <table id="iqcr8" style="box-sizing: border-box; height: 150px; margin: 0 auto 10px auto; padding: 5px 5px 5px 5px; width: 90%; max-width: 1920px;" width="90%" height="150">
+    <tbody style="box-sizing: border-box;">
+      <tr id="ik5nh" style="box-sizing: border-box;">
+        <td id="ink9g" style="box-sizing: border-box; font-size: 12px; font-weight: 300; vertical-align: top; color: rgb(111, 119, 125); margin: 0; padding: 0;" valign="top">
+          <table id="iaqck" style="box-sizing: border-box; height: 150px; margin: 0 auto 10px auto; padding: 5px 5px 5px 5px; width: 100%;" width="100%" height="150">
+            <tbody style="box-sizing: border-box;">
+              <tr style="box-sizing: border-box;">
+                <td id="iowcq" style="box-sizing: border-box; font-size: 12px; font-weight: 300; vertical-align: top; color: rgb(111, 119, 125); margin: 0; padding: 0 0 0 0;" valign="top">
+                  <table id="iuofw" style="box-sizing: border-box; height: 150px; margin: 0 auto 10px auto; padding: 5px 5px 5px 5px; width: 100%; background-color: #000000;" width="100%" height="150" bgcolor="#000000">
+                    <tbody id="i9g4w" style="box-sizing: border-box;">
+                      <tr style="box-sizing: border-box;">
+                        <td id="i0aao" style="box-sizing: border-box; font-size: 12px; font-weight: 300; vertical-align: middle; color: rgb(111, 119, 125); margin: 0; padding: 0 0 0 0; width: 50%; text-align: center;" width="50%" valign="middle" align="center">
+                          <img id="iucy8" src="https://nadil.generalsenses.com/images/nadil.png" style="box-sizing: border-box; color: black; width: 70%; max-width: 275px;">
+                        </td>
+                        <td id="ievor" style="box-sizing: border-box; font-size: 12px; font-weight: 300; vertical-align: top; color: rgb(111, 119, 125); margin: 0; padding: 0; width: 50%; text-align: center;" width="50%" valign="top" align="center">
+                          <img id="iqd5i" src="https://nadil.generalsenses.com/images/nadil-cutlery.png" style="box-sizing: border-box; color: black; width: 70%; max-width: 125px;">
+                        </td>
+                      </tr>
+                    </tbody>
+                  </table>
+                  <table id="i5322" style="box-sizing: border-box; height: 150px; margin: 0 auto 10px auto; padding: 5px 5px 5px 5px; width: 100%; background-color: #ffffff;" width="100%" height="150" bgcolor="#ffffff">
+                    <tbody style="box-sizing: border-box;">
+                      <tr style="box-sizing: border-box;">
+                        <td id="iiv3y" style="box-sizing: border-box; font-size: 12px; font-weight: 300; vertical-align: top; color: rgb(111, 119, 125); margin: 0; padding: 0; background-color: #ffffff;" valign="top" bgcolor="#ffffff">
+                          <table id="ike66" style="box-sizing: border-box; height: 150px; margin: 0 auto 10px auto; padding: 5px 5px 5px 5px; width: 100%; border-radius: 0 0 0 0;" width="100%" height="150">
+                            <tbody style="box-sizing: border-box;">
+                              <tr style="box-sizing: border-box;">
+                                <td id="iecux" style="box-sizing: border-box; font-size: 12px; font-weight: 300; vertical-align: top; color: rgb(111, 119, 125); margin: 0 0 0 0; padding: 0; background-color: #edeff0; border-radius: 80px 80px 80px 80px; min-height: 500px;" valign="top" bgcolor="#edeff0">
+                                  <div id="iw3tf" style="box-sizing: border-box; padding: 80px 10px 10px 10px; text-align: center; font-weight: 600; color: #000000; font-size: 1rem;">
+                                    <span style="box-sizing: border-box; font-family: Arial,Helvetica,sans-serif;"></span>HI WALEED
+                                    <br style="box-sizing: border-box;">
+                                  </div>
+                                  <div id="io4kn" style="box-sizing: border-box; padding: 60px 10px 10px 10px; text-align: center; font-weight: 600; color: #000000; font-size: 1rem;">YOUR BOOKING HAS BEEN CONFIRMED
+                                    <br style="box-sizing: border-box;">
+                                  </div>
+                                  <div id="id7e8" style="box-sizing: border-box; padding: 60px 10px 10px 10px; text-align: center; font-weight: 600; color: #000000; font-size: 1rem;">6 PEOPLE
+                                    <br style="box-sizing: border-box;">
+                                  </div>
+                                  <div id="i4q4j" style="box-sizing: border-box; padding: 60px 10px 10px 10px; text-align: center; font-weight: 600; color: #000000; font-size: 1rem;">7:00 PM
+                                    <br style="box-sizing: border-box;">
+                                  </div>
+                                  <div id="injdy" style="box-sizing: border-box; padding: 60px 10px 10px 10px; text-align: center; font-weight: 600; color: #000000; font-size: 1rem;">JANUARY 1 2023
+                                    <br style="box-sizing: border-box;">
+                                  </div>
+                                  <div id="i2w2d" style="box-sizing: border-box; padding: 60px 10px 80px 10px; text-align: center; font-weight: 600; color: #000000; font-size: 1rem;">OSKOYO/KUWAIT CITY
+                                    <br style="box-sizing: border-box;">
+                                  </div>
+                                </td>
+                              </tr>
+                            </tbody>
+                          </table>
+                          <table id="iqjjo" style="box-sizing: border-box; height: 150px; margin: 0 auto 10px auto; padding: 5px 5px 5px 5px; width: 100%;" width="100%" height="150">
+                            <tbody style="box-sizing: border-box;">
+                              <tr style="box-sizing: border-box;">
+                                <td id="ildsi" style="box-sizing: border-box; font-size: 12px; font-weight: 300; vertical-align: top; color: rgb(111, 119, 125); margin: 0; padding: 0; text-align: center;" valign="top" align="center">
+                                  <div id="ipakt" style="box-sizing: border-box; padding: 10px; text-align: center; font-size: large; font-style: italic; font-weight: 600;">"CURATED TO PROVIDE YOU WITH THE BEST"
+                                    <br style="box-sizing: border-box;">
+                                  </div>
+                                  <img id="i4n2h" src="https://nadil.generalsenses.com/images/insta-mail.png" style="box-sizing: border-box; color: black; width: 40px;" width="40">
+                                  <table id="i2w5a" style="box-sizing: border-box; height: 150px; margin: 0 auto 10px auto; padding: 5px 5px 5px 5px; width: 100%; background-color: #000000;" width="100%" height="150" bgcolor="#000000">
+                                    <tbody style="box-sizing: border-box;">
+                                      <tr style="box-sizing: border-box;">
+                                        <td id="iykbp" style="box-sizing: border-box; font-size: 12px; font-weight: 300; vertical-align: middle; color: rgb(111, 119, 125); margin: 0; padding: 0; width: 33.33%; text-align: center;" width="33.33%" valign="middle" align="center">
+                                          <a id="iuy3f" style="box-sizing: border-box; color: #ffffff; font-size: medium;">CANCELATION POLICY</a>
+                                        </td>
+                                        <td id="i0uyv" style="box-sizing: border-box; font-size: 12px; font-weight: 300; vertical-align: middle; color: rgb(111, 119, 125); margin: 0; padding: 0; width: 33.33%; text-align: center;" width="33.33%" valign="middle" align="center">
+                                          <a id="i2z7h" style="box-sizing: border-box; color: #ffffff; font-size: medium;">CONTACT US</a>
+                                        </td>
+                                        <td id="i9a1f" style="box-sizing: border-box; font-size: 12px; font-weight: 300; vertical-align: middle; color: rgb(111, 119, 125); margin: 0; padding: 0; width: 33.33%; text-align: center;" width="33.33%" valign="middle" align="center">
+                                          <a id="i92if" style="box-sizing: border-box; color: #ffffff; font-size: medium;">FAQ</a>
+                                        </td>
+                                        <td id="i41zk" style="box-sizing: border-box; font-size: 12px; font-weight: 300; vertical-align: top; color: rgb(111, 119, 125); margin: 0; padding: 0; width: 50%;" width="50%" valign="top">
+                                        </td>
+                                        <td id="i67mf" style="box-sizing: border-box; font-size: 12px; font-weight: 300; vertical-align: top; color: rgb(111, 119, 125); margin: 0; padding: 0; width: 50%;" width="50%" valign="top">
+                                        </td>
+                                        <td id="ik19n" style="box-sizing: border-box; font-size: 12px; font-weight: 300; vertical-align: top; color: rgb(111, 119, 125); margin: 0; padding: 0; width: 50%;" width="50%" valign="top">
+                                        </td>
+                                      </tr>
+                                    </tbody>
+                                  </table>
+                                </td>
+                              </tr>
+                            </tbody>
+                          </table>
+                        </td>
+                      </tr>
+                    </tbody>
+                  </table>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </td>
+      </tr>
+    </tbody>
+  </table>
+</body>
