f9f8dfbe7708ac06b76a713c4a16a58fe7728531
Reda Makarem <makarem.reda@gmail.com>
2023-01-19 03:29:53 +0300

    ..

--- resources/views/livewire/site/restaurant/menu.blade.php
+++ resources/views/livewire/site/restaurant/menu.blade.php
@@ -4,7 +4,7 @@
 
 
 <div>
-    <div class="bg-[#f5f5f5] min-h-[500px] h-full rounded-[64px] py-8 shadow-md scrollbar-hide max-h-[1030px] overflow-y-scroll">
+    <div class="bg-[#f5f5f5] min-h-[500px] h-full rounded-[64px] py-8 shadow-md scrollbar-hide max-h-[900px] overflow-y-scroll">
         @foreach($restaurant->menus[0]->categories as $category)
         <div class="category-container my-4">
             <div class="font-lato font-bold uppercase tracking-widest text-4xl text-center">{{  $category->name  }}</div>
