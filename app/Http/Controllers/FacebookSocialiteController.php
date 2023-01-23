b497b510875902cce32c5072f815a4b01e6c2aa7
Reda Makarem <makarem.reda@gmail.com>
2023-01-23 22:28:16 +0300

    ..

--- app/Http/Controllers/FacebookSocialiteController.php
+++ app/Http/Controllers/FacebookSocialiteController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Exception;
 use App\Models\User;
+use App\Models\Profile;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Laravel\Socialite\Facades\Socialite;
