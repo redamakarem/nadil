378b27301a8de8c82886c05ea9ed12e03569f7dc
Reda Makarem <makarem.reda@gmail.com>
2023-01-24 23:09:15 +0300

    ..

--- app/Listeners/NewSiteBookingListener.php
+++ app/Listeners/NewSiteBookingListener.php
@@ -3,9 +3,10 @@
 namespace App\Listeners;
 
 use App\Events\NewBookingEvent;
+use Illuminate\Support\Facades\Mail;
 use App\Mail\BookingConfirnationMail;
-use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
 
 class NewSiteBookingListener
 {
@@ -27,6 +28,6 @@ class NewSiteBookingListener
      */
     public function handle(\App\Events\NewBookingEvent $event)
     {
-        Mail::to($event->user->email)->send(new BookingConfirnationMail($event->user));
+        Mail::to($event->user->email)->send(new BookingConfirnationMail($event->user,$event->booking));
     }
 }
