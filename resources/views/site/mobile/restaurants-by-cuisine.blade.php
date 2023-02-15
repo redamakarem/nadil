43e185b4a86f6f20b0afb4048a6e45076c3625c6
Reda Makarem <makarem.reda@gmail.com>
2023-02-15 03:47:29 +0300

    ..

--- resources/views/site/mobile/restaurants-by-cuisine.blade.php
+++ resources/views/site/mobile/restaurants-by-cuisine.blade.php
@@ -8,8 +8,8 @@
                     <div class="item rounded-xl py-12 shadow-lg filter grayscale mx-6 my-4"
                         style="background-image:url('{{ $restaurant->getFirstMediaUrl('restaurant_images') }}'); background-size: cover">
                         <a href="{{ route('site.restaurants.view', ['id' => $restaurant->id]) }}">
-                            <h4 class="text-center">{{ $restaurant->{'name_' . app()->getLocale()} }}</h4>
-                            <div class="address text-center">{{ $restaurant->areaa->{'name_' . app()->getLocale()} }}</div>
+                            <h4 class="text-center font-lato font-normal rtl:font-ahlan rtl:tracking-normal text-white uppercase text-[26px] tracking-[2px]">{{ $restaurant->{'name_' . app()->getLocale()} }}</h4>
+                            <div class="address text-center font-lato rtl:font-ahlan rtl:tracking-normal text-white uppercase text-[18px] tracking-[2px]">{{ $restaurant->areaa->{'name_' . app()->getLocale()} }}</div>
                         </a>
                     </div>
                 @endforeach
