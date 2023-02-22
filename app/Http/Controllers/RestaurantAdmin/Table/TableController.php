63872fbf98d6816649666d0ab492edcc24e11990
Reda Makarem <makarem.reda@gmail.com>
2023-02-22 18:19:22 +0300

    ..

--- app/Http/Controllers/RestaurantAdmin/Table/TableController.php
+++ app/Http/Controllers/RestaurantAdmin/Table/TableController.php
@@ -10,6 +10,10 @@ class TableController extends Controller
 {
     public function index($restaurant_id)
     {
+        if(auth()->user()->hasRole('restaurant-super-admin')){
+            $restaurant = auth()->user()->restaurants->contains($restaurant_id)?Restaurant::find($restaurant_id):null;
+            return view('restaurant-admin.dining-tables.index',compact('restaurant'));
+        }
         $restaurant = Restaurant::findOrFail($restaurant_id);
         if(auth()->user()->workplace->id==$restaurant->id){
             return view('restaurant-admin.dining-tables.index',compact('restaurant'));
